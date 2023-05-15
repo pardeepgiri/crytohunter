384af9d1e21b10339cefcffb1a61316920d3c446
Pardeep Giri <108610241+pardeepgiri@users.noreply.github.com>
2023-05-15 23:33:34 +0530

    Update Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
 const Header = () => {
   return (
     <div className='mainheader'>
         <div className="leftheader">
-            <a href="/">Cryto_Hunter</a>
+            {/* <a href="/">Cryto_Hunter</a> */}
+            <Link to="/">Cryto_Hunter</Link>
         </div>
         <div className="rightheader">
-            <a href="/">List</a>
-            <a href="/">Home</a>
-            <a href="/">Prices</a>
+            <Link to="/">Home</Link>
+            <Link to="/list">List</Link>
+            <Link to="/list">Prices</Link>
         </div>
     </div>
   )
